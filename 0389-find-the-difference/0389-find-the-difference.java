281b93c6aacdf95a23161ba36e12e90c0f41025b
ankit <60979473+Ankit4201@users.noreply.github.com>
2023-10-01 22:25:54 +0530

    Time: 1 ms (100.00%), Space: 41 MB (20.52%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,22 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int freq1[]=new int[26];
-        int freq2[]=new int [26];
-        char missing='0';
-        for(int i=0; i<s.length(); i++)
+        int sum1=0, sum2=0;
+        for(char ch: s.toCharArray())
         {
-            freq1[s.charAt(i)-'a']++;
+            sum1+=ch;
         }
-        for(int i=0; i<t.length(); i++)
+        for(char ch: t.toCharArray())
         {
-            freq2[t.charAt(i)-'a']++;
+            sum2+=ch;
         }
-        for(int i=0; i<26; i++)
-        {
-            if(freq1[i]-freq2[i]!=0)
-                missing=(char)('a'+i);
-        }
-        return missing;
+        return (char)(sum2-sum1);
         
     }
 }
