a625160e548b215a3f271ca661ea745a8cb8c92c
ankit <60979473+Ankit4201@users.noreply.github.com>
2024-03-27 19:52:27 +0530

    Time: 2 ms (97.23%), Space: 44.6 MB (72.51%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] numberGame(int[] nums) {
        Arrays.sort(nums);
-      
-        int []num = nums.clone();
-        for(int i=0; i<num.length; i=i+2){
-            int temp = num[i];
-            num[i] = num[i+1];
-            num[i+1] = temp;
+        for(int i=0; i<nums.length; i=i+2){
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
         }
-        return num;
+        return nums;
       
         
     }
